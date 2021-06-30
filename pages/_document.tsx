389d839f10c47b9a5943ed82eece58a9132b812b
Yaroslav Lebedev <yaroslav.lebedev@ru.axept.com>
2021-07-01 00:10:35 +0800

    fix types

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,10 +2,10 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
-  static getInitialProps({ renderPage }) {
+  static getInitialProps({ renderPage }: any) {
     const sheet = new ServerStyleSheet()
     const page = renderPage(
-      (App) => (props) => sheet.collectStyles(<App {...props} />)
+      (App: any) => (props: any) => sheet.collectStyles(<App {...props} />)
     )
     const styleTags = sheet.getStyleElement()
     return { ...page, styleTags }
